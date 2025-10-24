398a84a760b8a51809662d411dc5abffecaf2f34
5771067 <5771067@qq.com>
2025-10-24 15:31:59 +0800

    不要压缩

--- main.py
+++ main.py
@@ -406,8 +406,6 @@ async def get_holders_info(contract_address: str,alphachainName,datalist=['top10
     headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
     'Accept': 'application/json',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'Accept-Encoding': 'gzip, deflate, br',
     'Connection': 'keep-alive',
 }
     try:

3bc6704f23e8ae2b819a9c53f886f9e40bafc925
5771067 <5771067@qq.com>
2026-01-16 15:50:09 +0800

    尝试买入所有新上的

--- alpha_diff_monitor.py
+++ alpha_diff_monitor.py
@@ -308,7 +308,8 @@ class AlphaDiffMonitor:
             logger.info(f"🎉 发现 {len(new_addresses)} 个新增 Alpha 代币!")
             new_addresses = sorted(new_addresses, key=lambda x: float(current_tokens[x]['fdv']))
 
-            new_addresses_buy = new_addresses[:2]
+            #new_addresses_buy = new_addresses[:2] #不能这样，有可能在里面有过滤掉一些
+            new_addresses_buy = new_addresses 
             for address in new_addresses_buy:
                 token_data = current_tokens[address]
                 # onlineTge = token_data.get('onlineTge', True)
